12e9bfcd13a8a3fc194bc5ceb517f08bbc6e280e
ismoilov223 <shukurulloismolilov832@gmail.com>
2023-07-09 11:12:53 +0500

    fix bag

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import countryRouter from "./routes/country.routes.js";
 const app = express();
-import dotev from "dotenv"
+import dotev from "dotenv";
 dotev.config();
-app.use(express.json({limit:"50mb"}));
+app.use(express.json({ limit: "50mb" }));
 app.use("/country", countryRouter);
-app.use("/", <h1>Server is live</h1>);
+app.use("/", (req, res) => {
+  res.send("server is live");
+});
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log("server is running...");

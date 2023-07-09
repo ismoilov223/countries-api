bd1d63cb6076b21cf14ddd618dda7d569f25b16f
ismoilov223 <shukurulloismolilov832@gmail.com>
2023-07-09 11:09:17 +0500

    add new route

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import dotev from "dotenv"
 dotev.config();
 app.use(express.json({limit:"50mb"}));
 app.use("/country", countryRouter);
+app.use("/", <h1>Server is live</h1>);
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log("server is running...");

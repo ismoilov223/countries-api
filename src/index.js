7f4b590118b34fb3d8ab75c858bedb698a713895
ismoilov223 <shukurulloismolilov832@gmail.com>
2023-07-09 10:56:38 +0500

    port change

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import countryRouter from "./routes/country.routes.js";
 const app = express();
-
+import dotev from "dotenv"
+dotev.config();
 app.use(express.json({limit:"50mb"}));
 app.use("/country", countryRouter);
-app.listen(8080, () => {
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
   console.log("server is running...");
 });
